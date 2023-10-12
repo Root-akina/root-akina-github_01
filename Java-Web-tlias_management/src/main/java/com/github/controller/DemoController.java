19d8a8b3dd2e0fc23ebea1c7d7946bdd72bfaa13
Root-Root <akinakdc3.7@gmail.com>
2023-10-12 12:18:58 +0800

    修改引入文件import

--- Java-Web-tlias_management/src/main/java/com/github/controller/DemoController.java
+++ Java-Web-tlias_management/src/main/java/com/github/controller/DemoController.java
@@ -1,7 +1,7 @@
 package com.github.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
