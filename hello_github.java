1f87fc0f53d56b7ef5016aef73592b0f527b1cd7
root <147520338+Root-akina@users.noreply.github.com>
2023-10-12 10:02:37 +0800

    Update hello_github.java
    hello from github

--- hello_github.java
+++ hello_github.java
@@ -1,5 +1,6 @@
 public class hello_github {
     public static void main(String[] args) {
         System.out.println("Hello World!  Hello Github");
+        System.out.println("Hello There!");
     }
 }
