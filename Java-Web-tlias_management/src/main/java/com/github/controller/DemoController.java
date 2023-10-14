a061a96ae2ea38e866b27fe8b8cafe225152395d
ROOT <akinakdc3.7@gmail.com>
2023-10-14 12:28:00 +0800

    contribute

--- Java-Web-tlias_management/src/main/java/com/github/controller/DemoController.java
+++ Java-Web-tlias_management/src/main/java/com/github/controller/DemoController.java
@@ -1,23 +1,28 @@
 package com.github.controller;
 
+import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoController {
 
     @GetMapping("/hello")
-    public void HelloWorld(){
+    public void HelloWorld() {
         System.out.println("Hello World");
     }
 
     @PostMapping("/depts")
-    public void depts(){
+    public void depts() {
         System.out.println("depts");
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteEmps(@PathVariable Integer id){
+    public void deleteEmps(@PathVariable Integer id) {
         System.out.println("deleted");
     }
-    
+
+    @PropertyMapping("/what")
+    public void what(){
+        System.out.println("why empty");
+    }
 }
